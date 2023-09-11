0f526365d81516459b3286a0e0e8a1c612177605
oskal314 <artem-osipovartem3002@mail.ru>
2023-09-11 03:50:58 +0300

    feat: Add Mail Service for sending activation emails
    Created a mail Service class that utilizes nodemailer to send activation emails.
    It configures the SMTP transporter using Gmail and the provided SMTP_USER and SMTP_PASSWORD.
    The sendActivationMail method sends an activation email with a link to the provided recipient.

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,5 +1,32 @@
-class MailService {
+const nodemailer = require('nodemailer')
+class mailService {
+
+  constructor() {
+    this.transporter = new nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
+      }
+    })
+  }
+
+  async sendActivationMail(to, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: 'Активация аккаунта на' + process.env.API_URL,
+      text: '',
+      html:
+        `
+        <div>
+          <h1>Для активации перейдите по ссылке</h1>
+          <a href="${link}">${link}</a>
+        </div>
+        `
+    })
+  }
 
 }
 
-module.exports = new MailService()
+module.exports = new mailService()
