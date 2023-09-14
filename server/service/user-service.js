8660137707748bce9c803460bfb78b289027d1eb
oskal314 <artem-osipovartem3002@mail.ru>
2023-09-14 03:55:14 +0300

    feat: Updated UserService
    - Added login functionality with user authentication.
    - Implemented logout functionality for revoking tokens.
    - Added token refresh functionality to extend user sessions.
    - Implemented a function to retrieve all users from the database.

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -33,6 +33,49 @@ class UserService {
     await user.save()
   }
 
+  async login(email, password) {
+    const user = await UserModel.findOne({ email })
+    if (!user) {
+      throw ApiError.badRequest('Пользователь с таким email не найден')
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password)
+    if (!isPassEquals) {
+      throw ApiError.badRequest('Неверный пароль')
+    }
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return { ...tokens, user: userDto }
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken)
+    return token
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.unauthorizedError()
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken)
+    const tokenFromDb = await tokenService.findToken(refreshToken)
+    if (!userData || !tokenFromDb) {
+      throw ApiError.unauthorizedError()
+    }
+    const user = await UserModel.findById(userData.id)
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return { ...tokens, user: userDto }
+  }
+
+  async getAllUsers() {
+    const users = await UserModel.find()
+    return users
+  }
+
 }
 
 module.exports = new UserService()
