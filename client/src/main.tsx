ded2fde802b42dda054194c60b9b47dd8c56c1ed
oskal314 <artem-osipovartem3002@mail.ru>
2023-09-19 04:19:40 +0300

    feat: Add state management with Store and Context
    Created a Store instance for managing application state and set up a Context provider to make the store accessible throughout the app.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import Store from './store/store.ts';
+
+interface StateStore {
+  store: Store;
+}
+
+const store = new Store();
+
+export const Context = createContext<StateStore>({
+  store,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Context.Provider
+    value={{
+      store,
+    }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Context.Provider>,
+);
