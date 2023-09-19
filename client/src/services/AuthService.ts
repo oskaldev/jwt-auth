39ce76cdd69d09c235058f9ee381d13c81be6aa6
oskal314 <artem-osipovartem3002@mail.ru>
2023-09-19 04:24:42 +0300

    feat: Updated logout method
    Changed the return type of the logout method in the UserService class to Promise<void> to indicate that it doesn't return any specific data.

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -11,7 +11,7 @@ export default class AuthService {
     return $api.post<AuthResponse>('/registration', { email, password })
   }
 
-  static async logout(): Promise<AxiosResponse<AuthResponse>> {
-    return $api.post<AuthResponse>('/logout')
+  static async logout(): Promise<void> {
+    return $api.post('/logout')
   }
 }
