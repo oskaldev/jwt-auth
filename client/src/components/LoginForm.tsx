0292bfa374f3cf64719bac473e69ee851ca4df8e
oskal314 <artem-osipovartem3002@mail.ru>
2023-09-20 23:44:46 +0300

    feat: add action login and registration

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,8 +1,10 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
+import { Context } from '../main'
 
 const LoginForm: FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const {store} = useContext(Context)
   return (
     <div>
       <input
@@ -17,8 +19,8 @@ const LoginForm: FC = () => {
         type='text'
         placeholder='Password'
       />
-      <button>Логин</button>
-      <button>Регистрация</button>
+      <button onClick={() => store.login(email, password)}>Логин</button>
+      <button onClick={() => store.registration(email, password)}>Регистрация</button>
     </div>
   );
 };
