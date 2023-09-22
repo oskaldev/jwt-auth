dcc371381815c6d021567ce158b4f2c97118750f
oskal314 <artem-osipovartem3002@mail.ru>
2023-09-22 17:18:07 +0300

    feat: Implement user data fetching functionality
    In this commit, a user data fetching functionality has been implemented. The main changes in this component include:
    
    - Added a `UserService` class to handle the fetching of user data from the API using Axios.
    
    - In the `App` component, a check for the presence of a token in local storage is performed on component load. If a token is found, the user is authenticated using the `store.checkAuth()` method.
    
    - Introduced a new button to trigger the retrieval of user data from the server when the user is authenticated. This functionality is provided by the `getUsers` function.
    
    - When the user is authenticated, their email is displayed on the page. Additionally, if the user's account is activated (based on `store.user.isActivated`), a message is shown indicating that their email has been confirmed.
    
    These changes enhance the `App` component's functionality by enabling it to fetch and display user data when the user is authenticated, providing a more complete user experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,58 @@
-import React, { FC } from 'react';
+/* eslint-disable react-refresh/only-export-components */
+import React, { FC, useContext, useEffect, useState } from 'react';
 import LoginForm from './components/LoginForm';
+import { Context } from './main';
+import { observer } from 'mobx-react-lite';
+import { IUser } from '../models/IUser';
+import UserService from './services/UserService';
 
 const App: FC = () => {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth();
+    }
+  }, [store]);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm />
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <LoginForm />
+      <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'Авторизуйтесь'}</h1>
+      <h1>
+        {store.user.isActivated ? `Почта подтверждена ${store.user.email}` : 'Подтвердите аккаунт!'}{' '}
+      </h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map(user => (
+        <div key={user.email}>{user.email}</div>
+      ))}
     </div>
   );
 };
 
-export default App;
+export default observer(App);
